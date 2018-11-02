4d9cfca18007bbd5299ea69b4ca4f961efaf788a
disha-wani <disha.wani@click2cloud.net>
2018-11-02 17:08:40 +0530

    add anti-ddos datasource

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -212,6 +212,7 @@ func Provider() terraform.ResourceProvider {
 			"huaweicloud_dms_az_v1":                 dataSourceDmsAZV1(),
 			"huaweicloud_dms_product_v1":            dataSourceDmsProductV1(),
 			"huaweicloud_dms_maintainwindow_v1":     dataSourceDmsMaintainWindowV1(),
+			"huaweicloud_antiddos_v1":               dataSourceAntiDdosV1(),
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
