23e238af4aa34e226ef5503a8e98570b38b74157
click2cloud-ninja-vlogs <c2cninja_vlogs@click2cloud.net>
2018-11-19 16:33:36 +0530

    add antiddos resource service

---
+++ huaweicloud/data_source_huaweicloud_antiddos_v1_test.go
@@ -0,0 +1,46 @@
+package huaweicloud
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccAntiDdosV1DataSource_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheckAntiddos(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccAntiDdosV1DataSource_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAntiDdosV1DataSourceID("data.huaweicloud_antiddos_v1.antiddos"),
+					resource.TestCheckResourceAttr("data.huaweicloud_antiddos_v1.antiddos", "floating_ip_id", OS_EIP_ID),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckAntiDdosV1DataSourceID(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Can't find defense status of EIP data source: %s ", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("Defense status of EIP data source ID not set")
+		}
+
+		return nil
+	}
+}
+
+var testAccAntiDdosV1DataSource_basic = fmt.Sprintf(`
+data "huaweicloud_antiddos_v1" "antiddos" {  
+  floating_ip_id = "%s"
+}
+`, OS_EIP_ID)
